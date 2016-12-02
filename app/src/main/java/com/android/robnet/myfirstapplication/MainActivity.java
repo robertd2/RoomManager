811ed7ac7114e0d06114f36d498c99a8f7a25489
rob <robert_d2@o2.pl>
2016-12-02 09:40:08 +0100

    NFC reader

--- app/src/main/java/com/android/robnet/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/android/robnet/myfirstapplication/MainActivity.java
@@ -1,6 +1,12 @@
 package com.android.robnet.myfirstapplication;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +30,9 @@ public class MainActivity extends Activity {
     private Layout layout;
     private WebSocketConnect wsConnect;
 
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+    private NfcAdapter mNfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +46,23 @@ public class MainActivity extends Activity {
         layout.setLayout(0);
         Log.i("rooms", "connecting to server...");
 
-        wsConnect = new WebSocketConnect(this, layout);
+        //wsConnect = new WebSocketConnect(this, layout);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            Log.i("NFC","NO ADAPTER AVAILABLE");
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            Log.i("NFC","NFC is disabled");
+        } else {
+            Log.i("NFC","NFC is enabled");
+        }
+
+        handleIntent(getIntent());
     }
 
     private void fullscreenCall() {
@@ -51,4 +76,74 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
+
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    @Override
+    protected void onPause() {
+        stopForegroundDispatch(this, mNfcAdapter);
+        super.onPause();
+    }
+
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Log.i("NFC","NFC card detected");
+            String type = intent.getType();
+            if (MIME_TEXT_PLAIN.equals(type)) {
+                Log.i("NFC","NFC data MIME type");
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new NdefReaderTask().execute(tag);
+
+            } else {
+                Log.d("NFC", "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techList = tag.getTechList();
+            String searchedTech = Ndef.class.getName();
+
+            for (String tech : techList) {
+                if (searchedTech.equals(tech)) {
+                    new NdefReaderTask().execute(tag);
+                    break;
+                }
+            }
+        }
+    }
 }
